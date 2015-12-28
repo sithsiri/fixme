f0b2dcc4b9884592826fb3dfa8865050495fd505
sithsiri <sith@live.com>
2015-12-28 11:16:34 -0600

    Dabble

--- FixMe/FixMe/Form1.vb
+++ FixMe/FixMe/Form1.vb
@@ -1,19 +1,6 @@
 ﻿Public Class Form1
-    Private Sub Button1_Click(ByVal sender As Object,
+    Private Sub Button1_Click(ByVal sender As System.Object,
         ByVal e As System.EventArgs) Handles Button1.Click
-        Dim psi As New _
-            System.Diagnostics.ProcessStartInfo("fixnet.bat")
-        psi.RedirectStandardOutput = True
-        psi.WindowStyle = ProcessWindowStyle.Hidden
-        psi.UseShellExecute = False
-        Dim listFiles As System.Diagnostics.Process
-        listFiles = System.Diagnostics.Process.Start(psi)
-        Dim myOutput As System.IO.StreamReader _
-            = listFiles.StandardOutput
-        listFiles.WaitForExit(2000)
-        If listFiles.HasExited Then
-            Dim output As String = myOutput.ReadToEnd
-            Debug.WriteLine(output)
-        End If
+        Process.Start("fixnet")
     End Sub
 End Class
