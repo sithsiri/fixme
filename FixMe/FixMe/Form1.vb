2305bb45949bac92392146a10d5fdfbd01eb1f38
sithsiri <sith@live.com>
2016-01-15 20:17:28 -0600

    k dis is all

--- FixMe/FixMe/Form1.vb
+++ FixMe/FixMe/Form1.vb
@@ -1,6 +1,8 @@
 ﻿Public Class Form1
-    Private Sub Button1_Click(ByVal sender As System.Object,
+
+    Private Sub Button1_Click(ByVal sender As Object,
         ByVal e As System.EventArgs) Handles Button1.Click
-        Process.Start("fixnet")
+        Process.Start("CMD", "/Q /C ipconfig /renew && ipconfig /flushdns && echo Press any key to finish... && pause > nul")
     End Sub
+
 End Class
